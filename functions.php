85a181547950649344cea2cdd6420f0d39e6c201
kjhank <sumerian.cry@gmail.com>
2020-01-07 20:08:05 +0100

    register spec post type

--- functions.php
+++ functions.php
@@ -1,4 +1,5 @@
 <?php
+
 if (!is_admin()) {
   wp_enqueue_style('main_css', get_template_directory_uri() . '/dist/style.min.css');
   wp_enqueue_script('main_js', get_template_directory_uri() . '/dist/app.min.js', null, null, true);
@@ -27,26 +28,55 @@ function register_menus()
 
 function create_members()
 {
+  $team_members_config = array(
+    'labels'  => array(
+      'name'          => __('Team members', 'en'),
+      'singular_name' => __('Team member', 'en'),
+      'add_new'       => __('Add new', 'en'),
+      'add_new_item'  => __('Add new team member', 'en'),
+      'edit_item'     => __('Edit person', 'en'),
+    ),
+    'public'      => true,
+    'has_archive' => false,
+    'rewrite'     => array(
+      'slug'  => 'team'
+    ),
+    'supports'    => array(
+      'title', 'custom-fields',
+    ),
+    'menu_icon'   => 'dashicons-groups',
+  );
+
+  $specializations_config = array(
+    'labels' => array(
+      'name'          => __('Specializations', 'en'),
+      'singular_name' => __('Specialization', 'en'),
+      'add_new'       => __('Add-new', 'en'),
+      'add_new_item'  => __('Add new specialization', 'en'),
+      'edit_item'     => __('Edit specialization', 'en')
+    ),
+    'public'      => true,
+    'has_archive' => false,
+    'rewrite'     => array(
+      'slug'  => 'spec'
+    ),
+    'supports'  => array(
+      'title', 'custom-fields'
+    ),
+    'taxonomies'  => array(
+      'category'
+    ),
+    'menu_icon' => 'dashicons-analytics',
+  );
+
   register_post_type(
     'team-members',
-    array(
-      'labels'      => array(
-        'name'          => __('Team members', 'en'),
-        'singluar_name' => __('Team member', 'en'),
-        'add_new'       => __('Add new', 'en'),
-        'add_new_item'  => __('Add new team member', 'en'),
-        'edit_item'     => __('Edit person', 'en'),
-      ),
-      'public'      => true,
-      'has_archive' => true,
-      'rewrite'     => array(
-        'slug'  => 'team'
-      ),
-      'supports'    => array(
-        'title', 'custom-fields',
-      ),
-      'menu_icon'   => 'dashicons-groups',
-    )
+    $team_members_config
+  );
+
+  register_post_type(
+    'specializations',
+    $specializations_config
   );
 }
 
@@ -58,34 +88,6 @@ function add_additional_class_on_li($classes, $item, $args)
 
   return $classes;
 }
-
-// /**
-//  * Have WordPress match postname to any of our public post types (post, page, race).
-//  * All of our public post types can have /post-name/ as the slug, so they need to be unique across all posts.
-//  * By default, WordPress only accounts for posts and pages where the slug is /post-name/.
-//  *
-//  * @param $query The current query.
-//  */
-// function gp_add_cpt_post_names_to_main_query( $query ) {
-// 	// Bail if this is not the main query.
-// 	if ( ! $query->is_main_query() ) {
-// 		return;
-// 	}
-// 	// Bail if this query doesn't match our very specific rewrite rule.
-// 	if ( ! isset( $query->query['page'] ) || 2 !== count( $query->query ) ) {
-// 		return;
-// 	}
-// 	// Bail if we're not querying based on the post name.
-// 	if ( empty( $query->query['name'] ) ) {
-// 		return;
-// 	}
-// 	// Add CPT to the list of post types WP will include when it queries based on the post name.
-// 	$query->set( 'post_type', array( 'post', 'page', 'team-members' ) );
-// }
-
-// wp_enqueue_script( 'tt-mobile-menu', get_template_directory_uri() . '/js/mobile-menu.js', array('jquery'), null, null, true );
-
-// add_action( 'pre_get_posts', 'gp_add_cpt_post_names_to_main_query' );
 
 add_action('init', 'optimize_loading');
 add_action('init', 'create_members');
