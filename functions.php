aaa498db78e634de9da749f486ac09cb57bbdfbd
kjhank <sumerian.cry@gmail.com>
2020-01-09 01:30:22 +0100

    add SVG support for file uploads

--- functions.php
+++ functions.php
@@ -19,6 +19,12 @@ function unload_block_library()
   wp_dequeue_style('wp-block-library');
 }
 
+function allow_svg_upload()
+{
+  $mimes['svg'] = 'image/svg+xml';
+  return $mimes;
+}
+
 function register_menus()
 {
   register_nav_menus(array(
@@ -95,6 +101,7 @@ add_action('init', 'register_menus');
 add_action('wp_enqueue_scripts', 'unload_block_library');
 
 add_filter('wp_enqueue_scripts', 'optimize_loading', PHP_INT_MAX);
+add_filter('upload_mimes', 'allow_svg_upload');
 add_filter('nav_menu_css_class', 'add_additional_class_on_li', 1, 3);
 add_filter('use_block_editor_for_posts', '__return_false', 10);
 add_filter('use_block_editor_for_post_type', '__return_false', 10);
