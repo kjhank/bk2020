00ba0704f507d0512e5bd2cf95b5a0d7a3b380ac
kjhank <sumerian.cry@gmail.com>
2020-01-09 01:29:48 +0100

    menu customization, too much work in php

--- src/js/index.js
+++ src/js/index.js
@@ -2,6 +2,7 @@ import Form from './FormElements';
 import LazyLoad from './LazyLoad';
 import MenuScroll from './MenuScroll';
 import smoothscroll from 'smoothscroll-polyfill';
+import CustomizeMenu from './CustomizeMenu';
 
 class Core {
   constructor() {
@@ -9,16 +10,19 @@ class Core {
       formSelector: '.contact-form__inner',
       lazyLoadSelector: '[data-background], [data-src]',
       menuSelector: '.home .main-header__navigation-item > a',
+      navSelector: '.main-header__navigation',
     }
 
     this.init();
   }
 
   init() {
-    const { formSelector, lazyLoadSelector, menuSelector } = this.config;
+    const { formSelector, lazyLoadSelector, menuSelector, navSelector } = this.config;
     new Form(formSelector);
     new LazyLoad(lazyLoadSelector);
     new MenuScroll(menuSelector);
+    new CustomizeMenu(navSelector);
+
     smoothscroll.polyfill();
   }
 }
