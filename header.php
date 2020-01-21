4c5d531dc3cae07fc2a41727d2dee3d1e1cffff8
kjhank <sumerian.cry@gmail.com>
2020-01-21 23:02:33 +0100

    change heading elements depending on page for SEO charlatans

--- header.php
+++ header.php
@@ -1,6 +1,7 @@
 <?php
 $home_url = get_home_url();
 $logo = get_field('main-logo', 'option');
+$menu = get_field('icon-menu', 'option');
 ?>
 <!doctype html>
 <html lang="<?= pll_current_language(); ?>">
@@ -23,22 +24,34 @@ $logo = get_field('main-logo', 'option');
 <body <?php body_class(); ?>>
   <header class="main-header">
     <div class="container main-header__wrapper">
+      <?php if (is_home() || is_front_page()): ?>
       <h1 class="main-header__logo">
         <a href="<?= $home_url; ?>" class="main-header__url">
-        <?= file_get_contents(get_attached_file($logo));?>
+          <?= file_get_contents(get_attached_file($logo)); ?>
         </a>
       </h1>
-      <?php
-      wp_nav_menu(array(
-        'menu'            => 'top-menu',
-        'theme-location'  => 'top-menu',
-        'container'       => 'nav',
-        'container_class' => 'main-header__navigation',
-        'fallback_cb'     => false,
-        'custom_li_class' => 'main-header__navigation-item',
-        'menu_class'      => 'main-header__navigation-inner',
-      ));
-      ?>
+      <?php else: ?>
+      <h2 class="main-header__logo">
+        <a href="<?= $home_url; ?>" class="main-header__url">
+          <?= file_get_contents(get_attached_file($logo)); ?>
+        </a>
+      </h2>
+      <?php endif;?>
+      <nav class="main-header__navigation">
+        <?php
+        wp_nav_menu(array(
+          'menu'            => 'top-menu',
+          'theme-location'  => 'top-menu',
+          'container'       => false,
+          'fallback_cb'     => false,
+          'custom_li_class' => 'main-header__navigation-item',
+          'menu_class'      => 'main-header__navigation-inner',
+        ));
+        ?>
+        <button class="main-header__navigation-toggle">
+          <?= file_get_contents(get_attached_file($menu)); ?>
+        </button>
+      </nav>
     </div>
   </header>
   <main class="main-content">
