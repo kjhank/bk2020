7f729431feceae4dc80b6601e1b1f35cffb51d22
kjhank <sumerian.cry@gmail.com>
2020-01-23 22:03:24 +0100

    add option for bright button

--- single-team-members.php
+++ single-team-members.php
@@ -6,6 +6,7 @@
 
 $data = get_field('member');
 $arrow = get_field('icon-arrow', 'option');
+$bright = $data['button']['bright'];
 ?>
 <section class="team-member" style="background-image: url(<?= $data['background-image']['url']; ?>);">
   <div class="container team-member__container">
@@ -14,7 +15,7 @@ $arrow = get_field('icon-arrow', 'option');
       <h1 class="team-member__name"><?= $data['name']; ?></h1>
       <div class="line line--highlight team-member__line"></div>
       <section class="team-member__description"><?= $data['description']; ?></section>
-      <a href="<?= $data['button']['url']; ?>" class="team-member__link button"><?= $data['button']['text']; ?><?= file_get_contents(get_attached_file($arrow)); ?></a>
+      <a href="<?= $data['button']['url']; ?>" class="team-member__link<?= $bright ? ' team-member__link--bright' : ''; ?> button"><?= $data['button']['text']; ?><?= file_get_contents(get_attached_file($arrow)); ?></a>
     </div>
   </div>
 </section>
