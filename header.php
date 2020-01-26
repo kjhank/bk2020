1ca2ff323099f24d39e98973b61578de733c448b
kjhank <sumerian.cry@gmail.com>
2020-01-26 17:41:03 +0100

    preliminary analytics code

--- header.php
+++ header.php
@@ -2,21 +2,26 @@
 $home_url = get_home_url();
 $logo = get_field('main-logo', 'option');
 $menu = get_field('icon-menu', 'option');
+$analytics = get_field('analytics', 'option');
 ?>
 <!doctype html>
 <html lang="<?= pll_current_language(); ?>">
 
 <head>
   <meta charset="utf-8">
-  <title><?php
-          if (is_home() || is_front_page()) {
-            echo bloginfo('name');
-          } else {
-            echo wp_title('');
-            echo ' &raquo; ';
-            echo bloginfo('name');
-          }
-          ?></title>
+  <title>
+    <?php
+    if (is_home() || is_front_page()) {
+      echo bloginfo('name');
+    } else {
+      echo wp_title('');
+      echo ' &raquo; ';
+      echo bloginfo('name');
+    }
+    ?></title>
+    <?php
+      echo '<script type="text/javascript" async defer>// ' . $analytics['ga-id'] . '</script>';
+    ;?>
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <?php wp_head(); ?>
 </head>
@@ -24,19 +29,19 @@ $menu = get_field('icon-menu', 'option');
 <body <?php body_class(); ?>>
   <header class="main-header">
     <div class="container main-header__wrapper">
-      <?php if (is_home() || is_front_page()): ?>
-      <h1 class="main-header__logo">
-        <a href="<?= $home_url; ?>" class="main-header__url">
-          <?= file_get_contents(get_attached_file($logo)); ?>
-        </a>
-      </h1>
-      <?php else: ?>
-      <h2 class="main-header__logo">
-        <a href="<?= $home_url; ?>" class="main-header__url">
-          <?= file_get_contents(get_attached_file($logo)); ?>
-        </a>
-      </h2>
-      <?php endif;?>
+      <?php if (is_home() || is_front_page()) : ?>
+        <h1 class="main-header__logo">
+          <a href="<?= $home_url; ?>" class="main-header__url">
+            <?= file_get_contents(get_attached_file($logo)); ?>
+          </a>
+        </h1>
+      <?php else : ?>
+        <h2 class="main-header__logo">
+          <a href="<?= $home_url; ?>" class="main-header__url">
+            <?= file_get_contents(get_attached_file($logo)); ?>
+          </a>
+        </h2>
+      <?php endif; ?>
       <nav class="main-header__navigation">
         <?php
         wp_nav_menu(array(
